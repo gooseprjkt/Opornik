46c36f57e8be6de63a5a226979e090a8321f0fc5
gooseprjkt <winpocostudios@gmail.com>
2025-12-30 20:52:04 +0300

    Update section_screen.dart

--- lib/screens/section_screen.dart
+++ lib/screens/section_screen.dart
@@ -39,9 +39,9 @@ class _SectionScreenState extends State<SectionScreen> {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
           } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
+            return Center(child: Text('Ошибка: ${snapshot.error}'));
           } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return const Center(child: Text('No rules available'));
+            return const Center(child: Text('Правил нет :('));
           } else {
             final rules = snapshot.data!;
             return ListView.builder(
